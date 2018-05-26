93fd0ae2cf12351253cfa381670a5b0ce0255088
ellery <acclist@qq.com>
2018-05-27 02:45:07 +0800

    修改默认的代码结构

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/reset.css'
+// 解决移动端1px问题
+import './assets/border.css'
 
 Vue.config.productionTip = false
+// 解决移动端使用click事件时,会延迟300ms后才执行的问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
