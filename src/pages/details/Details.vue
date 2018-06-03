969b957a09f44dc1d732c678f47379a01cf46d5b
ellery <acclist@qq.com>
2018-06-03 15:17:01 +0800

    递归组件的使用

--- src/pages/details/Details.vue
+++ src/pages/details/Details.vue
@@ -2,6 +2,7 @@
   <div>
     <details-banner></details-banner>
     <details-header></details-header>
+    <details-list :list="list"></details-list>
     <div style="height: 50rem;"></div>
   </div>
 </template>
@@ -9,11 +10,32 @@
 <script>
 import DetailsBanner from './components/Banner'
 import DetailsHeader from './components/Header'
+import DetailsList from './components/List'
 export default {
   name: 'Details',
   components: {
     'details-banner': DetailsBanner,
-    'details-header': DetailsHeader
+    'details-header': DetailsHeader,
+    'details-list': DetailsList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '第三层'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }]
+    }
   }
   // TODO: 这里还要完成activated生命周期钩子的内容,内容为根据不同的id加载不同的详情页
 }
---
+++ src/pages/details/components/List.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="item"
+         v-for="(item, index) of list"
+         :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <!-- 递归组件-->
+      <div v-if="item.children" class="item-children">
+        <details-list :list="item.children"></details-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailsList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    height .8rem
+    line-height .8rem
+    font-size .32rem
+    padding 0 .2rem
+    position relative
+    text-indent .4rem
+    .item-title-icon
+      display inline-block
+      position absolute
+      top .2rem
+      left .2rem
+      width .36rem
+      height .36rem
+      background url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+      margin-right .1rem
+      background-size .4rem 3rem
+  .item-children
+    padding 0 .2rem
+</style>
