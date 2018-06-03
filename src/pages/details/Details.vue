aace5f9356f94122833460c3e40ed3855c161e2b
ellery <acclist@qq.com>
2018-06-03 13:51:05 +0800

    完成详情页Header的代码编写

--- src/pages/details/Details.vue
+++ src/pages/details/Details.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <details-banner></details-banner>
+    <details-header></details-header>
+    <div style="height: 50rem;"></div>
   </div>
 </template>
 
 <script>
 import DetailsBanner from './components/Banner'
+import DetailsHeader from './components/Header'
 export default {
   name: 'Details',
   components: {
-    'details-banner': DetailsBanner
+    'details-banner': DetailsBanner,
+    'details-header': DetailsHeader
   }
+  // TODO: 这里还要完成activated生命周期钩子的内容,内容为根据不同的id加载不同的详情页
 }
 </script>
 
---
+++ src/pages/details/components/Header.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <router-link tag="div"
+                 to="/"
+                 class="header-abs"
+                 v-show="showAbs">
+      <span class="iconfont header-abs-back">&#xe624;</span>
+    </router-link>
+    <div class="header-fixed"
+         v-show="!showAbs"
+         :style="opacityStyle">
+      景点详情
+      <router-link tag="span" to="/" class="iconfont header-fixed-back">&#xe624;</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailsHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      // console.log('scroll')
+      const top = document.documentElement.scrollTop
+      // if (top < 60) {
+      //   this.showAbs = true
+      // } else if (top >= 60 && top < 140) {
+      //   const opacity = top / 140
+      //   this.opacityStyle = { opacity }
+      //   this.showAbs = false
+      // }
+
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    // 将事件绑定在全局对象上会影响到其他组件(这里应该很好理解)
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  // 当当前组件被停止时,解绑全局对象上的scroll事件,这样就不会污染到其他组件了
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/variables.styl'
+  .header-abs
+    position absolute
+    left .2rem
+    top .2rem
+    width .8rem
+    height .8rem
+    line-height .8rem
+    text-align center
+    border-radius 50%
+    background rgba(0, 0, 0, .8)
+    .header-abs-back
+      color #fff
+      font-size .4rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right  0
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    font-size .32rem
+    background $bgColor
+    .header-fixed-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      font-size .4rem
+</style>
